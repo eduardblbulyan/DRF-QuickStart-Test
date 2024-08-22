e48d9898966356c83f7c20b2c3b81463279ca5cc
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-22 22:13:28 +0400

    Adding views

--- tutor/quickstart/views.py
+++ tutor/quickstart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from tutor.quickstart.serializers import UserSerializer, GroupSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint that allows groups to be viewed or edites
+    """
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint that allows groups to be viewed or edites
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
