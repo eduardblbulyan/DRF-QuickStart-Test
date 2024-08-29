f704199bb40f55064d455e80d585a06f10bd0c20
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-30 02:12:33 +0400

    Usage comments

--- serializer_tut/snippets/models.py
+++ serializer_tut/snippets/models.py
@@ -17,3 +17,39 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+
+
+# usage in shell (serialization)
+"""
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
+
+serializer = SnippetSerializer(snippet)
+serializer.data # --> {'id': 2, 'title': '', 'code': 'print("hello, world")\n', 'linenos': False, 'language': 'python', 'style': 'friendly'}
+content = JSONRenderer().render(serializer.data)
+content
+"""
+
+# deserialize
+"""
+import io
+
+stream = io.BytesIO(content)
+data = JSONParser().parse(stream)
+serializer = SnippetSerializer(data=data)
+serializer.is_valid()
+# True
+serializer.validated_data
+# {'title': '', 'code': 'print("hello, world")', 'linenos': False, 'language': 'python', 'style': 'friendly'}
+serializer.save()
+# <Snippet: Snippet object>
+
+"""
