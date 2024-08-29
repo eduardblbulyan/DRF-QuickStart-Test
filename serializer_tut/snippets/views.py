4aa4eec6a32301da8e8e1909ff75ce1f67cc7cad
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-30 02:28:38 +0400

    View for detailed snippet

--- serializer_tut/snippets/views.py
+++ serializer_tut/snippets/views.py
@@ -21,3 +21,30 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+    
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except:
+        return HttpResponse(status=404)
+    
+    if request.method == "GET":
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
